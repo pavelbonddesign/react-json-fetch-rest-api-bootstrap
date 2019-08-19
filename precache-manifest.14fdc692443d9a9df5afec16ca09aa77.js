54de97f97c27ea6d2dec606db7384a6b98bd5961
Pavel <pasharadio@yahoo.com>
2019-08-19 14:20:53 +0300

    Add files via upload

---
+++ precache-manifest.14fdc692443d9a9df5afec16ca09aa77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeec01e78fca266a573a4ae346a3cd83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73f5fd4e7def011c3eac",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b531de06d15f88fc10dc",
+    "url": "/static/js/2.bf08e785.chunk.js"
+  },
+  {
+    "revision": "73f5fd4e7def011c3eac",
+    "url": "/static/js/main.382599e1.chunk.js"
+  },
+  {
+    "revision": "14acd668c81a41e366f3",
+    "url": "/static/js/runtime~main.848c12ba.js"
+  }
+]);
